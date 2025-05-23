d63b059c43e8124df43fb99f33739e426d2278c8
sanislearning <sanjaybiju10@gmail.com>
2025-05-24 03:08:49 +0530

    Added the retrieval functions, harded coded implementation of consine similarity

--- dataset.py
+++ dataset.py
@@ -25,3 +25,22 @@ def add_chunk_to_database(chunk):
 for i, chunk in enumerate(dataset):
     add_chunk_to_database(chunk)
     print(f'Added chunk {i+1}/{len(dataset)} to the database')
+
+#Next we will deal with the implemenatation of the retrieval function
+
+def cosine_similarity(a,b):
+    dot_product=sum([x*y for x,y in zip(a,b)])
+    norm_a=sum([x**2 for x in a])**0.5
+    norm_b=sum([x**2 for x in b])**0.5
+    return dot_product/(norm_a*norm_b)
+    #https://www.youtube.com/watch?v=e9U0QAFbfLI, check this out if you wanna figure out implementation
+
+def retrieve(query,top_n=3):
+    query_embedding=ollama.embed(model=embedding_model,input=query)['embeddings'][0]
+    similarities=[] #Creates a list that will store tupes of (chunk, similarity_score)
+    for chunk,embedding in vectorDB:
+        similarity=cosine_similarity(query_embedding,embedding)
+        similarities.append((chunk,similarity))
+        #sorting by similarity in desecending order, higher the value of similarity the more relevant the chunk
+        similarities.sort(key=lambda x:x[1], reverse=True)
+        return similarities[:top_n]
